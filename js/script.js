67dd90e88fb7994d7aadc619bfe56068b7c19d77
basrunki <basrunkisiburian@gmail.com>
2022-01-27 16:36:43 +0700

    sbrcode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+  strings: ["", "Junior Web Developer", "Student", "Junior Web Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
